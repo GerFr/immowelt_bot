8117e60198f78f701c725b284e76d89af4c13251
Gerrit F <140012598+GerFr@users.noreply.github.com>
2023-12-08 14:22:50 +0000

    added newest raspi version

---
+++ scraper_immo.py
@@ -0,0 +1,103 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import json
+import random
+
+def get_titles(request_content: bytes)->object:
+    soup = BeautifulSoup(request_content, 'html.parser')
+    text = str(soup.find_all("h2"))
+    text_seperated = [title.strip("</h2>").strip("[</h2>") for title in text.split("</h2>, ") if "[TAUSCHWOHNUNG]" not in title and "Wohnungsswap" not in title and not "class=" in title]
+    formatted_text = ""
+    for i, title in enumerate(text_seperated):
+        formatted_text += f"{i+1:3}: {title}\n"
+    return formatted_text
+
+
+def get_data(request_content: bytes)->list:
+    soup = BeautifulSoup(request_content, 'html.parser')
+    return extract_jsons_from_string(str(soup))
+
+
+def extract_jsons_from_string(data_string: str)-> list:
+    first_index = []
+    second_index = []
+    valid_jsons = {}
+    single_jsons = {}
+    
+    #get all possible indeces
+    for index, char in enumerate(data_string):
+        if char == '{':
+            first_index.append(index)
+        elif char == '}':
+            second_index.append(index) 
+              
+    #get all indeces in right order 
+    combinations = [(first, second) for second in second_index for first in first_index if first < second]
+
+    #get all possible valid jsons, valid jsons in nested in larder jsons also get created
+    for combination in combinations:
+        string_range = data_string[combination[0]:combination[1]+1]
+        try:
+            json_range = json.loads(string_range)
+            valid_jsons[string_range]=json_range
+        except Exception as e:
+            pass
+    
+    #get only outermost jsons from all valid jsons
+    for json_key in valid_jsons.keys():
+        add = True
+        for other_key in valid_jsons.keys():
+            if json_key in other_key and json_key != other_key:
+                add = False
+        if add: single_jsons[json_key] = valid_jsons[json_key]
+    return list(single_jsons.values())
+
+
+def get_local_jsons(directoy: str)-> list:
+    text = ""  
+    with open(directoy) as file:
+        for line in file:
+            text += line
+    return extract_jsons_from_string(text)
+
+
+def largest_json(list)-> dict:
+    return max(list, key=lambda item: len(item.keys()))
+
+def get_small_txt():
+    r = requests.get("https://www.immowelt.de/suche/hamburg-ottensen/wohnungen/mieten?lat=53.5513&lon=9.92234&sr=3")
+    request_content = r.content.decode('utf-8','ignore')
+	#immo_data = largest_json(get_data(request_content))
+	#print(list(immo_data.keys()))
+    return get_titles(request_content)
+	#with open("remote_json.json", "w") as file:
+	#    json.dump(immo_data, file)
+
+
+#bot stuff
+TOKEN = ""
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
+
+application = ApplicationBuilder().token(TOKEN).build()
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="I'm a bot, please talk to me!"
+    )
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=get_small_txt())
+
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', start)
+
+echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+application.add_handler(start_handler)
+application.add_handler(echo_handler)
+
+application.run_polling()
